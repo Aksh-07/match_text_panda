cbf887a2030168f8c2e6b94deb614b0d5c209775
Aksh <akshdeep434@gmail.com>
2022-04-21 03:23:11 +0530

    model for updating newly added files

--- main.py
+++ main.py
@@ -3,18 +3,39 @@ import os
 
 files_list = os.listdir("all_files")
 
-files = {}
-for file in files_list:
-    data = pd.read_csv(f"all_files/{file}", sep="delimiter", header=None, engine="python").to_string().lower()
-    files[file] = data
+
+def store_file_data():
+    files = {}
+    for file in files_list:
+        files[file] = pd.read_csv(f"all_files/{file}", sep="delimiter", header=None,
+                                  engine="python").to_string().lower()
+    return files
+
+
+file_data = store_file_data()
+
+
+def update_data(files_data):
+    all_files_list = os.listdir("all_files/")
+    new_files = {}
+
+    for file in all_files_list:
+        if file not in files_data.keys():
+            new_files[file] = pd.read_csv(
+                f"all_files/{file}", sep="delimiter", header=None, engine="python"
+            ).to_string().lower()
+
+    for k, v in new_files.items():
+        files_data[k] = v
+    return files_data
+
 
 while True:
     s = input("Enter String to search for: ").lower()
+    updated_data = update_data(file_data)
     if s == "quit":
         break
     else:
-        for key, value in files.items():
-            print(f"Match found in {key}, Total Occurrence: {value.count(s)}")\
+        for key, value in updated_data.items():
+            print(f"Match found in {key}, Total Occurrence: {value.count(s)}") \
                 if s in value else print(f"No Match found in {key}")
-
-
