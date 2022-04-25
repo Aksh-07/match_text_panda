c4381aff3e98a5904e6056400c8b3cc6935c4fa9
Aksh <akshdeep434@gmail.com>
2022-04-25 07:27:09 +0530

    code refactoring for module structure and using os.stat

--- store_file_data.py
+++ store_file_data.py
@@ -13,6 +13,7 @@ class StoreData:
         for file in self.files_list:
             self.files[file] = pd.read_csv(f"all_files/{file}", sep="delimiter", header=None,
                                            engine="python").to_string().lower()
-            self.dates[file] = os.path.getmtime(f"all_files/{file}")
+            # self.dates[file] = os.path.getmtime(f"all_files/{file}")
+            self.dates[file] = os.stat(f"all_files/{file}").st_mtime
         return self.files, self.dates
 
